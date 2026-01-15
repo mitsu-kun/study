6110282a8fd1c1f9caf1f19620435c937ec3567f
mitsu-kun <k.kagerou715@gmail.com>
2026-01-15 12:27:24 +0900

    イベント発火 基礎編

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,2 +1,16 @@
-let lists = document.getElementsByClassName("pull-down-list")
-console.log(lists)
+const pullDownButton = document.getElementById("lists");
+
+lists.addEventListener('mouseover',function() {
+    const pullDownButton = document.getElementById("lists")
+    console.log("乗る")
+})
+
+lists.addEventListener('mouseout',function() {
+    const pullDownButton = document.getElementById("lists")
+    console.log("外れる")
+})
+
+lists.addEventListener('click',function() {
+    const pullDownButton = document.getElementById("lists")
+    console.log("クリック")
+})
