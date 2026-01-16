a9465601eae25966fee8739743a9ca428190d542
mitsu-kun <k.kagerou715@gmail.com>
2026-01-17 07:57:23 +0900

    イベント発火 基礎編
    setAttributeとremoveAttributeによってマウスが乗る・離れる・クリック時に「リスト」ボタンの色変更を実装

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -14,3 +14,15 @@ lists.addEventListener('click',function() {
     const pullDownButton = document.getElementById("lists")
     console.log("クリック")
 })
+
+pullDownButton.addEventListener("mouseover",function(){
+    pullDownButton.setAttribute("style", "background-color:blue;")
+})
+
+pullDownButton.addEventListener("mouseout",function(){
+    pullDownButton.removeAttribute("style", "background-color:red;")
+})
+
+pullDownButton.addEventListener("click",function(){
+    pullDownButton.setAttribute("style", "background-color:green;")
+})
