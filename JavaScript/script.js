f9abe13c63a38905d076c82cade58f211aee086d
mitsu-kun <k.kagerou715@gmail.com>
2026-01-21 13:04:03 +0900

    文字列の取得編
    クリック時文字列取得
    リスト選択でリンク先へ遷移実装

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,19 @@
+function pullDown() {
 const pullDownButton = document.getElementById("lists");
 const pullDownParents = document.getElementById("pull-down");
+const pullDownChild = document.querySelectorAll(".pull-down-list")
+const currentList = document.getElementById("current-list");
+const url = [
+    "https://www.google.com/",
+    "https://www.google.com/",
+    "https://www.google.com/",
+    "https://www.google.com/",
+]
+pullDownChild.forEach((list, index) => {
+    list.addEventListener('click', () => {
+        window.location.href = url[index];
+    });
+});
 
 pullDownButton.addEventListener('mouseover',function() {
     this.setAttribute("style", "background-color:blue;")
@@ -13,6 +27,16 @@ pullDownButton.addEventListener('click',function() {
     if (pullDownParents.getAttribute("style") == "display:block;"){
         pullDownParents.removeAttribute("style", "display:block;")
     } else {
-        pullDownParents.setAttribute("style", "display:block;")
+        pullDownParents.setAttribute("style", "display:block;")       
     }
 })
+
+pullDownChild.forEach(function(list){
+    list.addEventListener("click", function(){
+        const value = list.innerHTML
+        currentList.innerHTML = value
+        console.log(value)
+    })
+})
+}
+window.addEventListener('load', pullDown)
